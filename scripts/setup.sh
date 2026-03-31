93e32e4125820096a6f18bb546448542698a77c1
henriquekon <henriqueandrekonrad@gmail.com>
2026-03-31 19:51:37 -0300

    erro na instalação no setup.sh corrigido

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -3,8 +3,8 @@
 sudo apt update
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common git
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyr>
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download>
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
