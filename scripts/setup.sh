efcfd6e2c5454905eb63ff786d5ca14fffd857a9
henriquekon <henriqueandrekonrad@gmail.com>
2026-03-31 11:58:53 -0300

    atualizado setup.sh para simplificá-lo

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y apt-transport-https ca-certificates curl software-properties-common git
+sudo apt install -y docker.io docker-compose-plugin git
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo systemctl start docker
+sudo systemctl enable docker
 
 sudo usermod -aG docker $USER
 
 git clone git@github.com:henriquekon/GC_aula06.git
 cd GC_aula06
 
-docker compose up -d
+sudo docker compose up -d
