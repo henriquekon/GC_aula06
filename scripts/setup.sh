44833e10506f4f8e371db8ac82616fe69e9bc749
henriquekon <henriqueandrekonrad@gmail.com>
2026-03-31 12:10:16 -0300

    baixar docker oficial

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y docker.io docker-compose-plugin git
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common git
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
 sudo systemctl start docker
 sudo systemctl enable docker
@@ -11,4 +17,4 @@ sudo usermod -aG docker $USER
 git clone git@github.com:henriquekon/GC_aula06.git
 cd GC_aula06
 
-sudo docker compose up -d
+docker compose up -d
