4bb5a332913f2ff2ac5251aec02dcb07c2062ef3
henriquekon <henriqueandrekonrad@gmail.com>
2026-05-04 16:48:49 -0300

    patch: sys.path

--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,7 @@ from unittest.mock import patch, MagicMock
 import sys
 import os
 
-sys.path.insert(0, os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..', 'api'))
 
 
 @pytest.fixture(scope="session")
